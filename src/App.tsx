0d90fbe960872ec96a404ed7784b04e70a5a0076
3mr <amrgouda127@gmail.com>
2024-12-16 01:30:44 +0200

    handle product.name bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,31 @@ import Button from "./components/UI/Button";
 import Input from "./components/UI/Input";
 import Modal from "./components/UI/Modal";
 import { formInputsList, productList } from "./data";
-import { useState } from "react";
-
+import { ChangeEvent, useState } from "react";
+import { IProduct } from "./interfaces";
 
 const App = () => {
   // ** state
+  const [product, setProduct] = useState<IProduct>({
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
+    colors: [],
+    category: {
+      name: "",
+      imageURL: "",
+    },
+  });
   const [isOpen, setIsOpen] = useState(false);
 
   // ** Handlers
-  function open() {
-    setIsOpen(true);
-  }
-
-  function close() {
-    setIsOpen(false);
-  }
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
 
   // ** Renders
   const renderProductList = () => {
@@ -29,13 +39,13 @@ const App = () => {
       </>
     );
   };
-
+  console.log(product);
   const renderFormInputList = formInputsList.map((input) => (
     <div className="flex flex-col space-y-2">
       <label className="text-sm font-medium text-gray-700 mb-[2px]">
         {input.label}
       </label>
-      <Input type="text"/>
+      <Input type="text" value={product[input.name]} name={input.name} onChange={(e) => onChangeHandler(e)} />
     </div>
   ));
 
